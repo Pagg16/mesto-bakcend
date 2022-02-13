9daa4334437550ef2881dd7735662fee1b7ccbf0
Pavel Rocket <pagg16pagg@gmail.com>
2022-02-13 12:06:22 +0300

    version 0.1.20

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -12,10 +12,12 @@ const allowedCors = [
 ];
 
 module.exports = function cors(req, res, next) {
+  console.log('1');
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.send('корсы раотают');
     res.header('Access-Control-Allow-Origin', origin);
+    console.log('1');
     return res.end();
   }
   const { method } = req;
@@ -25,7 +27,30 @@ module.exports = function cors(req, res, next) {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    console.log('1');
     return res.end();
   }
   return next();
 };
+
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,POST,PUT,PATCH,DELETE';
+
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
+
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+
+//     return res.end();
+//   }
+
+//   return next();
+// };
